56958554ad9e862e849cc9ed78ad5740d1185b5b
David Manpearl <dmanpearl@pixelmonks.com>
2025-03-26 13:37:29 -0700

    chore(proc): remove stale commented out code

--- proc/proc.go
+++ proc/proc.go
@@ -7,23 +7,7 @@ type result struct {
 
 func Process(vals []int, groupSize int) []result {
 	var results []result
-
-	// Print all values in the input slice
-	// for i, val := range vals {
-	// 	fmt.Printf("%d. %v\n", i, val)
-	// }
-
-	// Print the sum of the next groupSize values in the input slice
-	// (not the requirement)
-	// for i := 0; i < len(vals)-groupSize; i++ {
-	// 	sum := 0
-	// 	for j := 0; j < groupSize; j++ {
-	// 		sum += vals[i+j]
-	// 	}
-	// 	fmt.Printf("%d. %v\n", i, sum)
-	// }
-
-	// Print the sum of each group of groupSize values. indicate groups that could not satisfy the groupSize quantity.
+	// Generate the sum of each group of groupSize values. indicate groups that could not satisfy the groupSize quantity.
 	groups := (len(vals) + groupSize - 1) / groupSize
 	for group := 0; group < groups; group++ {
 		i := group * 3
@@ -36,11 +20,6 @@ func Process(vals []int, groupSize int) []result {
 			}
 			sum += vals[i+j]
 		}
-		// status := "complete"
-		// if !whole {
-		// 	status = "partial"
-		// }
-		// fmt.Printf("%d. %v %v\n", group, sum, status)
 		results = append(results, result{Sum: sum, Whole: whole})
 	}
 	return results
